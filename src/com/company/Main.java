50bd0b0222310c65849571b5315c29d3e443b267
mkherchaoui <m.kherchaoui@yahoo.fr>
2016-06-07 16:16:53 +0200

    initialisation du projet

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println ("2400 est bissextile:"+isBissextile(2400));
+	// write your code here
+    }
+    public static boolean isBissextile (int annee)  {
+            boolean resultat= false;
+        if (annee%4==0){
+            if (annee%100==0){
+                if (annee%400==0){
+                    resultat = true;
+                }
+            }
+            else{
+                resultat=true;
+            }
+        }
+
+     return resultat;
+    }
+
+}
